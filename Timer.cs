ec70f862bd45f8acd78e46d60ab1315be99f8e6c
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2019-02-12 15:07:34 -0200

    Added namespace

--- Timer.cs
+++ Timer.cs
@@ -1,97 +1,100 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
-public class Timer : MonoBehaviour {
 
-    [SerializeField] bool _isRegressive;
-    [SerializeField] float _totalTime;
-    [SerializeField] float _timeLeft;
-
-    public float TimeLeft
+namespace _2MuchPines.Unity_Timer
+{
+    public class Timer : MonoBehaviour
     {
-        get
+
+        [SerializeField] bool _isRegressive;
+        [SerializeField] float _totalTime;
+        [SerializeField] float _timeLeft;
+
+        public float TimeLeft
         {
-            return _timeLeft;
-        }
-    }
-
-    [SerializeField] bool _timeOver;
-    [SerializeField] bool _startOnAwake;
-    [SerializeField] UnityEvent _onTimeOver;
-    bool _isRunning;
-
-    #region MonoBehavior Methods
-
-    void Awake()
-    {
-        ResetTimer();
-    }
-
-    // Use this for initialization
-    void Start () {
-        if (_startOnAwake)
-            StartRunning();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        updateTime();
-    }
-
-    #endregion
-
-    #region Public Members
-    public void StartRunning()
-    {
-        _isRunning = true;
-    }
-
-    public void StopRunning()
-    {
-        _isRunning = false;
-    }
-
-    public bool ToggleRunning()
-    {
-        _isRunning = !_isRunning;
-        return _isRunning;
-    }
-
-    public void ResetTimer()
-    {
-        _timeLeft = _isRegressive ? _totalTime : 0;
-    }
-    #endregion
-
-    void SetTimeOver()
-    {
-        _timeOver = true;
-        StopRunning();
-        _onTimeOver.Invoke();
-    }
-
-    void updateTime()
-    {
-        if (_isRunning)
-        {
-            if (_isRegressive)
+            get
             {
-                _timeLeft -= Time.deltaTime;
-                if (_timeLeft <= 0.1)
-                {
-                    SetTimeOver();
-                }
-            }
-            else
-            {
-                _timeLeft += Time.deltaTime;
-                if (_timeLeft > _totalTime)
-                {
-                    SetTimeOver();
-                }
+                return _timeLeft;
             }
         }
-    }
 
+        [SerializeField] bool _timeOver;
+        [SerializeField] bool _startOnAwake;
+        [SerializeField] UnityEvent _onTimeOver;
+        bool _isRunning;
+
+        #region MonoBehavior Methods
+
+        void Awake()
+        {
+            ResetTimer();
+        }
+
+        void Start()
+        {
+            if (_startOnAwake)
+                StartRunning();
+        }
+
+        void Update()
+        {
+            UpdateTime();
+        }
+
+        #endregion
+
+        #region Public Members
+        public void StartRunning()
+        {
+            _isRunning = true;
+        }
+
+        public void StopRunning()
+        {
+            _isRunning = false;
+        }
+
+        public bool ToggleRunning()
+        {
+            _isRunning = !_isRunning;
+            return _isRunning;
+        }
+
+        public void ResetTimer()
+        {
+            _timeLeft = _isRegressive ? _totalTime : 0;
+        }
+        #endregion
+
+        void SetTimeOver()
+        {
+            _timeOver = true;
+            StopRunning();
+            _onTimeOver.Invoke();
+        }
+
+        void UpdateTime()
+        {
+            if (_isRunning)
+            {
+                if (_isRegressive)
+                {
+                    _timeLeft -= Time.deltaTime;
+                    if (_timeLeft <= 0.1)
+                    {
+                        SetTimeOver();
+                    }
+                }
+                else
+                {
+                    _timeLeft += Time.deltaTime;
+                    if (_timeLeft > _totalTime)
+                    {
+                        SetTimeOver();
+                    }
+                }
+            }
+        }
+
+    }
 }
